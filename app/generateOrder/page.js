ef63c7cb49b90e80804aa726a4e1c4001beab4d3
Hamza-js <mhamzaawanmp1@gmail.com>
2023-04-10 16:20:02 -0700

    changes

--- app/generateOrder/page.js
+++ app/generateOrder/page.js
@@ -4,23 +4,29 @@ import { usePathname } from "next/navigation";
 import Link from "next/link";
 import QRCode from "react-qr-code";
 import Header from "../../components/Header";
+import { ChevronRightIcon, ChevronLeftIcon } from "@heroicons/react/24/solid";
 
 export default function selectSize() {
   const pathname = usePathname();
 
   return (
     <div className="main">
-      <div className="innerContainerWhite px-5 flex flex-col justify-between">
+      <div className="innerContainerWhite flex flex-col justify-between">
         <Header url={pathname} />
-        <div>
-          <h2 className="heading">Your Results</h2>
-          <p className="text pb-6">Show this at the counter</p>
-
+        <div className="px-8 mt-[60px]">
+          <div className="flex items-center justify-center flex-col text-center">
+            <h2 className="text-black font-semibold text-2xl font-sans py-4 ">
+              Your Result
+            </h2>
+            <p className="text-[#60656E] text-sm font-medium w-[275px] h-[54px]">
+              Show this at the counter
+            </p>
+          </div>
           <QRCode
             value="Walnut, Ce, Apple, Orange, Milk"
             size={256}
             style={{
-              height: 280,
+              height: 168,
               marginBottom: 20,
               maxWidth: "100%",
               width: "100%",
@@ -28,31 +34,61 @@ export default function selectSize() {
             viewBox={`0 0 256 256`}
           />
 
-          <div className="flex flex-col gap-y-4 w-full">
-            <div className="flex w-full">
-              <h3 className="w-1/4 text-black font-bold text-lg">Other</h3>
-              <p className="w-full text-black text-lg">Walnut</p>
+          <div className="rounded-xl border border-gray-400-500 mt-[25px]">
+            <div className="flex flex-row justify-between p-2">
+              <p className="font-medium text-sm">Calories</p>
+              <p className="font-medium text-sm">240 cal</p>
             </div>
+            <div className="flex flex-col rounded-xl bg-[#7264E4] text-white justify-between pb-[30px] mb-[30px] mt-[8px] px-3 w-full">
+              <div className="flex flex-col w-full py-3 border-b-2 border-solid border-[#9186EA] border-opacity-22">
+                <p className=" font-semibold text-base">Base</p>
+                <div className="flex flex-row w-full justify-between">
+                  <p className=" font-normal text-xs">Almond Milk</p>
+                  <p className=" font-normal text-xs">20 Cal</p>
+                </div>
+              </div>
 
-            <div className="flex w-full">
-              <h3 className="w-1/4 text-black font-bold text-lg">Dry</h3>
-              <p className="w-full text-black text-lg">Ce</p>
-            </div>
+              <div className="flex flex-col w-full py-3 border-b-2 border-solid border-[#9186EA] border-opacity-22">
+                <p className=" font-semibold text-base">Fruit</p>
+                <div className="flex flex-row w-full justify-between">
+                  <p className=" font-normal text-xs">Apple</p>
+                  <p className=" font-normal text-xs">20 Cal</p>
+                </div>
+                <div className="flex flex-row w-full justify-between">
+                  <p className=" font-normal text-xs">Orange</p>
+                  <p className=" font-normal text-xs">20 Cal</p>
+                </div>
+              </div>
 
-            <div className="flex w-full">
-              <h3 className="w-1/4 text-black font-bold text-lg">Fruit</h3>
-              <p className="w-full text-black text-lg">Apple, Orange</p>
-            </div>
+              <div className="flex flex-col w-full py-3 border-b-2 border-solid border-[#9186EA] border-opacity-22">
+                <p className=" font-semibold text-base">Dry</p>
+                <div className="flex flex-row w-full justify-between">
+                  <p className=" font-normal text-xs">20 Cal</p>
+                  <p className=" font-normal text-xs">20 Cal</p>
+                </div>
+              </div>
 
-            <div className="flex w-full">
-              <h3 className="w-1/4 text-black font-bold text-lg">Base</h3>
-              <p className="w-full text-black text-lg">Milk</p>
+              <div className="flex flex-col w-full pt-3 pb-7">
+                <p className=" font-semibold text-base">Other</p>
+                <div className="flex flex-row w-full justify-between">
+                  <p className=" font-normal text-xs">Walnut</p>
+                  <p className=" font-normal text-xs">20 Cal</p>
+                </div>
+              </div>
+
+              <button className="bg-white hover:bg-[#5e53b5] rounded-full w-full h-[45px] text-[#7264E4] font-medium text-base">
+                Shuffle
+              </button>
             </div>
           </div>
         </div>
-        <div className="mt-6">
-          <Link href={"/"}>
-            <button className="button mb-3">Return</button>
+
+        <div className=" w-full h-[120px] bg-[#7264E4] rounded-t-[25px] z-[1]">
+          <Link href={`/`}>
+            <div className="bg-white text-black hover:bg-[#5e53b5] hover:text-white mx-[18px] my-[30px] py-[11px] flex justify-center items-center rounded-[18px] cursor-pointer">
+              <p className="text-[18px] font-medium ">New Order</p>
+              <ChevronRightIcon className="h-7 w-7 pl-1" />
+            </div>
           </Link>
         </div>
       </div>
--- app/page.js
+++ app/page.js
@@ -2,6 +2,7 @@ import Link from "next/link";
 import Image from "next/image";
 import BD from "../images/Home/BG.png";
 import logo from "../images/Home/logo.png";
+import { ChevronRightIcon, ChevronLeftIcon } from "@heroicons/react/24/solid";
 
 export default function Home() {
   return (
@@ -21,8 +22,10 @@ export default function Home() {
 
         <div className=" w-full h-[120px] bg-white rounded-t-[25px] z-[1]">
           <Link href={`/requirements`}>
-            <div className="bg-[#7264E4] hover:bg-[#5e53b5] mx-[18px] my-[30px] py-[11px] flex justify-center items-center rounded-[18px] cursor-pointer">
-              <p className="text-[18px] font-medium text-white">Continue</p>
+            <div className="bg-[#7264E4] hover:bg-[#5e53b5] text-white mx-[18px] my-[30px] py-[11px] flex justify-center items-center rounded-[18px] cursor-pointer">
+              <p className="text-[18px] font-medium ">Continue</p>
+              <ChevronRightIcon className="h-7 w-7 pl-1" />
+
             </div>
           </Link>
         </div>
