161696a84c1dde86e3c86ecbc5cd16f474250640
Hamza-js <mhamzaawanmp1@gmail.com>
2023-04-29 05:36:58 -0700

    d

--- app/generateOrder/page.js
+++ app/generateOrder/page.js
@@ -54,7 +54,9 @@ export default function selectSize() {
           <div className="rounded-xl border border-gray-400-500 mt-[25px] sm:h-[30vh] sm:my-[0px] mb-[25px]">
             <div className="flex flex-row justify-between sm:py-[4px] p-2">
               <p className="font-medium text-sm">Calories</p>
-              <p className="font-medium text-sm">{result.sum}</p>
+              {result.sum && (
+                <p className="font-medium text-sm">{result.sum}</p>
+              )}
             </div>
             <div className="flex flex-col rounded-xl bg-[#7264E4] text-white justify-between sm:items-center pb-[10px] mt-[8px] sm:mt-[6px] px-3 w-full">
               <div className="flex flex-col w-full py-3 sm:py-1 border-b-2 border-solid border-[#9186EA] border-opacity-22">
